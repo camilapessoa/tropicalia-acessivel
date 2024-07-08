d453faae6e6d046dd58df222c4ce5ee4cc12037f
camilapessoa <camilabentespessoa@gmail.com>
2024-07-08 15:16:00 -0300

    grid

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const accessibilityBtn = document.getElementById('accessibility-btn');
+    const accessibilityOptions = document.getElementById('accessibility-options');
     const increaseFontBtn = document.getElementById('increase-font');
     const resetFontBtn = document.getElementById('reset-font');
     const decreaseFontBtn = document.getElementById('decrease-font');
     const toggleContrastBtn = document.getElementById('toggle-contrast');
+
+    accessibilityBtn.addEventListener('click', () => {
+        accessibilityOptions.classList.toggle('show');
+    });
 
     increaseFontBtn.addEventListener('click', () => {
         document.body.style.fontSize = '1.25rem';
@@ -20,6 +26,5 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.classList.toggle('high-contrast');
     });
 });
-
 
 
