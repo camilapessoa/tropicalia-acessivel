cb90640e916b6c86f1757157209c8d2f11b9211c
camilapessoa <camilabentespessoa@gmail.com>
2024-07-08 10:55:26 -0300

    teste

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const increaseFontBtn = document.getElementById('increase-font');
     const resetFontBtn = document.getElementById('reset-font');
     const decreaseFontBtn = document.getElementById('decrease-font');
+    const toggleContrastBtn = document.getElementById('toggle-contrast');
 
     increaseFontBtn.addEventListener('click', () => {
         document.body.style.fontSize = '1.25rem';
@@ -14,6 +15,11 @@ document.addEventListener('DOMContentLoaded', () => {
     decreaseFontBtn.addEventListener('click', () => {
         document.body.style.fontSize = '0.875rem';
     });
+
+    toggleContrastBtn.addEventListener('click', () => {
+        document.body.classList.toggle('high-contrast');
+    });
 });
+
 
 
